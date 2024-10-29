2d8897c258be7565f5ecea8152da208a2c05db26
JCTOVARU <jctovarueda@gmail.com>
2024-10-29 07:49:12 -0500

    Agregar el ejercicio de la Sesion N° 6

--- Clase06-Integracion-APIs/api_consumo_demo_1/lib/main.dart
+++ Clase06-Integracion-APIs/api_consumo_demo_1/lib/main.dart
@@ -68,64 +68,80 @@ class _PantallaGaleriaFotosState extends State<PantallaGaleriaFotos> {
 
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Galeria de Fotos'),
+        title: const Text('ISIL - Galeria de Fotos'),
       ),
       body: estaCargando
       ? const Center(
         child: CircularProgressIndicator(),
       ) :
-      /*Column(
-        children: [*/
-          /*
+      Column(
+        children: [
           ElevatedButton(
             onPressed: () async {
               await cargarData();
             },
             child: const Text('Refrescar'),
           ),
-          */
-          GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            ),
-            itemCount: photos.length,
-            itemBuilder: (context, index) {
-              final photo = photos[index];
-              return Card(
-                child: Column(
-                  children: [
-                    Image.network(
-                      photo['thumbnailUrl'],
-                      fit: BoxFit.cover,
-                      height: 100,
-                      width: 100,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Image.network(
-                                  'https://i0.wp.com/viajarparavivir.com/wp-content/uploads/2017/02/Logo-ISIL.png',
-                                  fit: BoxFit.cover,
-                                  height: 100,
-                                  width: 100
-                                );
-                      }
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        photo['title'],
-                        style: const TextStyle(fontSize: 12),
-                        textAlign: TextAlign.center,
+          Expanded(
+            child: GridView.builder(
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
                       ),
-                    ),
-                  ]
-                ),
-              );
-            },
+                      itemCount: photos.length,
+                      itemBuilder: (context, index) {
+                        final photo = photos[index];
+                        return Card(
+                          child: Column(
+                            children: [
+                              Image.network(
+                                photo['thumbnailUrl'],
+                                loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                                  if (loadingProgress == null) {
+                                    // Si loadingProgress es null, significa que la imagen ya terminó de cargarse.
+                                    return child;
+                                  } else {
+                                    // Mientras la imagen se carga, muestra un indicador de progreso.
+                                    return Center(
+                                      child: CircularProgressIndicator(
+                                        value: loadingProgress.expectedTotalBytes != null
+                                            ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
+                                            : null,
+                                      ),
+                                    );
+                                  }
+                                },
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Image.network(
+                                            'https://i0.wp.com/viajarparavivir.com/wp-content/uploads/2017/02/Logo-ISIL.png',
+                                            fit: BoxFit.cover,
+                                            height: 100,
+                                            width: 100
+                                          );
+                                },
+                                fit: BoxFit.cover,
+                                height: 100,
+                                width: 100
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  photo['title'],
+                                  style: const TextStyle(fontSize: 12),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ]
+                          ),
+                        );
+                      },
+                  ),
           ),
-        /*],
-      ),*/
+        ],
+      ),
     );
 
 
